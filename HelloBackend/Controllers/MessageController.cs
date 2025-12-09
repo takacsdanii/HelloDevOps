f0685f212e54c4e95db6e37a3d2eaf676e8f6b8e
TakacsDaniel-FGHJG9 <takacs.daniel.attila02@gmail.com>
2025-12-09 02:05:06 +0100

    Initial commit of HelloDevOps API and Angular project

---
+++ HelloBackend/Controllers/MessageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloBackend.Controllers
+{
+    [Route("api/messages")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var message = "Hello DevOps World!";
+            return Ok(message);
+        }
+    }
+}
