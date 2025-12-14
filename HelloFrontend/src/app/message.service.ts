e99300e651b7af744ded6ce793d9d686ca13ee7f
takacsdanii <takacs.daniel.attila02@gmail.com>
2025-12-15 00:51:59 +0100

    environments configured properly

--- HelloFrontend/src/app/message.service.ts
+++ HelloFrontend/src/app/message.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
+  private baseUrl = `${environment.apiUrl}/api/messages`;
 
   constructor(private http: HttpClient) { }
 
   public getMessage(): Observable<string> {
-    const url = 'https://localhost:7257/api/messages';
+    const url = `${this.baseUrl}`;
     return this.http.get(url, { responseType: 'text' });
   }
 }
