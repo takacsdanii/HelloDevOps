bc1040880d2f15ad3f5b5e2b986b598d33c86e03
takacsdanii <takacs.daniel.attila02@gmail.com>
2025-12-15 16:47:34 +0100

    img build commands extended

--- scripts/build_frontend_image.ps1
+++ scripts/build_frontend_image.ps1
@@ -1,1 +1,3 @@
 docker build -t hello-frontend:latest -f HelloFrontend/Dockerfile .
+docker build -t hello-frontend-dev:latest -f HelloFrontend/Dockerfile.dev .
+docker build -t hello-frontend-prod:latest -f HelloFrontend/Dockerfile.prod .
